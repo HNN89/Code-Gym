4245119f3d40169c1162c8955196656291fdf3ea
HNN89 <95356342+HNN89@users.noreply.github.com>
2022-02-22 15:59:48 +0700

    Add files via upload

---
+++ Lesson_8-2.py
@@ -0,0 +1,7 @@
+num=float(input("Hay nhap mot so ban thich "))
+if num%2==0:
+    print("So ban nhap la mot so chan")
+elif num%2==1:
+    print("So ban nhap laf mot so le")
+else:
+    print("So ban nhap khong phai la so tu nhien")
